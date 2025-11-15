a6d9d81a6e96498f9fc01624a54f1b983ebcf256
Mouad Nassri <mouadstev1@gmail.com>
2025-11-15 17:02:42 +0100

    initial commit (add tailwind)

---
+++ README.md
@@ -0,0 +1,1 @@
+GUI application dedicated for users only
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./core.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center pt-8">
+      <h1 className="text-2xl font-bold ">E-goodies client area project</h1>
+    </div>
+  );
+}
+
+export default App;
